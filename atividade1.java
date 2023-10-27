370e7a4a0df413cca4358263fa688305822c6677
Eduudc16 <eduardodacosta162003@gmail.com>
2023-10-26 21:44:20 -0300

    ssasasasasasasasas

--- atividade1.java
+++ atividade1.java
@@ -2,61 +2,74 @@ import java.util.Scanner;
 
 public class atividade1 {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int escolha;
-
-        do {
-            System.out.println("\nEscolha uma opção:");
-            System.out.println("1 - Calcular a área de um círculo");
-            System.out.println("2 - Calcular o volume de um cone");
-            System.out.println("3 - Sair");
-
-            escolha = scanner.nextInt();
-
-            switch (escolha) {
-                case 1:
-                    calcularAreaDoCirculo();
-                    break;
-                case 2:
-                    calcularVolumeDoCone();
-                    break;
-                case 3:
-                    System.out.println("Saindo do programa. Adeus!");
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-            }
-        } while (escolha != 3);
-
-        scanner.close();
+    public static double areacirculo (double raio) {
+        double area;
+        area = Math.PI * (raio * raio);
+        return area;    
     }
 
-    public static void calcularAreaDoCirculo() {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o raio do círculo: ");
-        double raio = scanner.nextDouble();
+     public static double volumedocone (double raiocone , double altura) {
+        double Volume;
 
-        double area = Math.PI * Math.pow(raio, 2);
-        System.out.println("A área do círculo é: " + area);
-
-        scanner.close();
-    }
-
-    public static void calcularVolumeDoCone() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite o raio da base do cone: ");
-        double raio = scanner.nextDouble();
-
-        System.out.print("Digite a altura do cone: ");
-        double altura = scanner.nextDouble();
-
-        double volume = (1.0 / 3.0) * Math.PI * Math.pow(raio, 2) * altura;
+        double volume = (1.0 / 3.0) * Math.PI * (raiocone * raiocone)* altura;
         System.out.println("O volume do cone é: " + volume);
 
-        scanner.close();
+        return volume;
+
+     }
+
+
+    
+
+    public static void main(String[] args) {
+       
+        int opcao=0;
+        while(opcao !=3) {
+           
+            System.out.println("Digite a Opcao desejada:");
+            System.out.println(" 1 - Caulculo da Area do Circulo");
+            System.out.println(" 2 - Caulculo do Volume do come");
+            System.out.println(" 3 - SAIR");
+            Scanner resp = new Scanner(System.in);
+            opcao = resp.nextInt();
+
+            switch(opcao) {
+
+                case 1:
+
+                System.out.println("Digite Raio do circulo: ");
+                int raio = resp.nextInt();
+
+                double resultado = areacirculo(raio);
+                System.out.println("Resultado");
+                System.out.println(resultado);
+                break;
+
+                case 2:
+                
+                 System.out.print("Digite o raio da base do cone: ");
+                 double raiocone = resp.nextDouble();
+                 System.out.print("Digite a altura do cone: ");
+                 double altura = resp.nextDouble();
+                 
+                 double resultado2 = volumedocone(raiocone,altura);
+                 System.out.println("Resultado");
+                  System.out.println(resultado2);
+
+
+                break;
+
+                case 3:
+
+                break;
+
+            
+
+        }
     }
+}
+
+
+
 }
---
+++ atividadedf.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class atividadedf {
+
+    public static void main(String[] args) {
+        
+   Scanner scanner = new Scanner(System.in);  
+
+
+
+
+
+
+
+
+
+
+
+   
+    }
+    
+}
